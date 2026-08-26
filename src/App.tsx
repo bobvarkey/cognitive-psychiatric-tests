f7a0c1606257a444d60938a4b9eca649eb4a49fe
Bob Varkey <72114170+bobvarkey@users.noreply.github.com>
2026-08-26 05:42:35 +0000

    make language toggle global and improve mobile controls

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { OfflineIndicator } from "@/components/OfflineIndicator";
 import { OfflineProvider } from "@/contexts/OfflineContext";
 import { useEffect } from "react";
 import { useThemeStore } from "@/hooks/useThemeStore";
+import { LanguageToggle } from "@/components/LanguageToggle";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -66,6 +67,7 @@ const App = () => (
                   <Route path="/assessment/:id" element={<Index />} />
                   <Route path="*" element={<NotFound />} />
                 </Routes>
+                <LanguageToggle />
                 <NavigationButtons />
                 <OfflineIndicator />
               </BrowserRouter>
